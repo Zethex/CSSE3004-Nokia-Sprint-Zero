b5715f742cc6909393a8501266396899eee8e33b
Dane Cousins <uqdcousi@uq.edu.au>
2012-04-06 21:59:36 +1000

    Tag Factory.h modified to add functions and variables

--- tagfactory.h
+++ tagfactory.h
@@ -1,10 +1,20 @@
 #ifndef TAGFACTORY_H
 #define TAGFACTORY_H
 
+#include "tag.h"
+#include "xmlmanager.h"
+
 class TagFactory
 {
 public:
-    TagFactory();
+    TagFactory get_instance();
+    Tag * get_tag_array();
+    void set_tag_array(Tag * tag_array);
+    void print();
+private:
+    Tag * tag_array;
+    XMLManager * xml;
+    // HAVE TO GET right map thing hereint process_tags(Map * dict);
 };
 
 #endif // TAGFACTORY_H
