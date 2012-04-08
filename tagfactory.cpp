de15cb2b7341f4c68c82919a79dc89d2b11aa1c9
Dane Cousins <uqdcousi@uq.edu.au>
2012-04-08 20:02:45 +1000

    Some changes to TagFactory to update stuff

--- tagfactory.cpp
+++ tagfactory.cpp
@@ -9,17 +9,21 @@ Tag * TagFactory::get_tag_array() {
     return this->tag_array;
 }
 
+void TagFactory::set_fileTagData(multimap<String, String> ftd) {
+    this->fileTagData = ftd;
+}
+
+multimap<String, String> TagFactory::get_fileTagData() {
+    return this->fileTagData;
+}
 
 /**
   Returns a tag factory by acting as its own constructor so there is only one copy
   **/
-TagFactory TagFactory::get_instance() {
-
+TagFactory TagFactory::get_instance(String filepath) {
     TagFactory *tf = new TagFactory();
-    //FileReader *fr = new FileReader("what do we put here");
-
-
-
-
+    FileReader *fr = new FileReader(filepath);
+    fr->read_file();
+    this->set_fileTagData(fr->get_multimap());
     return *tf;
 }
--- tagfactory.h
+++ tagfactory.h
@@ -11,14 +11,17 @@
 class TagFactory
 {
 public:
-    TagFactory get_instance();
+    TagFactory get_instance(String filepath);
     Tag * get_tag_array();
     void set_tag_array(Tag * tag_array);
     void print();
+    void set_fileTagData(multimap<String, String> ftd);
+    multimap<String, String> get_fileTagData();
 private:
     Tag * tag_array;
     XMLManager * xml;
     int process_tags(multimap<string , FILE*> * dict);
+    multimap<String, String> fileTagData;
 };
 
 #endif // TAGFACTORY_H
