4515a511ed79f2868ec37d1197815d74bc8810e6
Anna Brown <ania.brown@gmail.com>
2012-04-10 15:18:52 +1000

    oops

--- sphere.h
+++ sphere.h
@@ -2,8 +2,9 @@
 #define SPHERE_H
 
 #include "face.h"
+
+#include <string>
 #include <QGraphicsScene> // fix - may need to move
-#include <QGraphicsEmbedScene> // fix - may need to move
 
 class Sphere
 {
@@ -11,7 +12,6 @@ public:
     Sphere();
     void createSphere(void);
     Face getface(int pos);
-
 
 private:
     Face faces[500]; // fix - terrible way to do this - add malloc when i have time
@@ -22,9 +22,9 @@ private:
     QVector3D cartesian(float x, float y, float z);
     void generateFileTextures(void);
 
-    QGraphicsScene scene[n_filenames]; // fix - most likely need to move to renderer
-    int textureId[n_filenames]; // fix - most likely need to move to renderer
-    string filenames[] = {"label 1", "label 2", "label3", "label 4", "label 5", "label 6", "label 7", "label 8"}; //fix - remove - only for testing
+    //QGraphicsScene scene[500]; // fix - most likely need to move to renderer
+    //int textureId[500]; // fix - most likely need to move to renderer
+    //string filenames[] = {"label 1", "label 2", "label3", "label 4", "label 5", "label 6", "label 7", "label 8"}; //fix - remove - only for testing
 };
 
 #endif // SPHERE_H
